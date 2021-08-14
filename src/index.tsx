febed3b75ab99d572c416a531fb500581fd6a854
Andrew Yang <andrewhuiyang0830@gmail.com>
2021-08-14 13:35:46 -0700

    added react-snap seo

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import GlobalStyle from "./GlobalStyle";
+import { hydrate, render } from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyle />
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+if (rootElement!.hasChildNodes()) {
+  hydrate([ <GlobalStyle />, <App /> ], rootElement);
+} else {
+  render([ <GlobalStyle />, <App /> ], rootElement);
+}
